270602f06059c0331a24dc3aab1c530d0dea6e8d
rheaacharya77 <ria.acharya07@gmail.com>
2020-08-12 03:47:55 +0545

    complete todo

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 var  todolist = document.querySelector("#todolist");
 var input = document.querySelector("#todoInput");
 
-
 function addTask() {
    var input_box = document.getElementById("todoInput").value
-    if(input_box ==''){
+    if(input_box ===''){
+   
         alert('you must write something');
-    } else{
-   input_date = document.getElementById("input_date");
+        return    
+    } 
    var input_text = document.createTextNode(input_box)
    var newItem = document.createElement("li")
    newItem.appendChild(input_text)
-   document.getElementById("todolist").appendChild(newItem)
+   // document.getElementById("todolist").appendChild(newItem)
+    todolist.appendChild(newItem)
+    todolist.insertBefore(newItem, todolist.firstChild);
    input.value ='';
-}
 
-};
+   };
+   
+   
+   todolist.onclick = function deleteTask(e) {
+    let remove = e.target.parentNode;
+    let parentNode = remove.parentNode;
+    parentNode.removeChild(remove);
+  }
+  
+
+
 
 todolist.onclick = function(data){
-   if(data.target.tagName == 'LI'){
+    if(data.target.tagName == 'LI'){
+        console.log("target",data);
+        console.log(data.target.parent);
         data.target.classList.toggle('checked');
-   }
+      //   todolist.innerHTML = "Paragraph changed!";
+   
+}
 };
+
+
